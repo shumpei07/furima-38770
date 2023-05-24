3850c460dc73bc202b6eef1eb77928cd4f39d200
shumpei07 <shumpeikonno07@gmail.com>
2023-05-24 17:25:38 +0900

    :imageの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   #  end
 
    def item_params
-     params.require(:item).merge(user_id: current_user.id).permit(:name,:introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :user_id)
+     params.require(:item).merge(user_id: current_user.id).permit(:name,:introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :user_id, :image)
    end
   
 end 
