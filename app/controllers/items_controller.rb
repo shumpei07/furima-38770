eb486ab9ec8df6e91b6d72da6a4f1398a5e7a30c
shumpei07 <shumpeikonno07@gmail.com>
2023-05-24 17:49:29 +0900

    :user_idの消去

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   #  end
 
    def item_params
-     params.require(:item).merge(user_id: current_user.id).permit(:name,:introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :user_id, :image)
+     params.require(:item).permit(:name,:introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :image).merge(user_id: current_user.id)
    end
   
 end 
