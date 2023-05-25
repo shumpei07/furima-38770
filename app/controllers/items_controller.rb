52affa55d21464e79fa30ace1df3f76e8a31eb99
shumpei07 <shumpeikonno07@gmail.com>
2023-05-25 20:57:18 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new,:edit]
-  before_action :@item = Item.find(params[:id]), only:[:show, :edit, :update]
+  before_action :set_item, only:[:show, :edit, :update]
 
    def index
     @items = Item.order("created_at DESC")
@@ -43,6 +43,7 @@ class ItemsController < ApplicationController
      params.require(:item).permit(:name,:introduction, :category_id, :item_condition_id, :postage_payer_id, :prefecture_id, :preparation_day_id, :price, :image).merge(user_id: current_user.id)
    end
   
-   def @item = Item.find(params[:id])
+   def set_item
+    @item = Item.find(params[:id])
    end
 end 
