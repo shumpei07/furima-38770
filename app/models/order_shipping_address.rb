65f9dde237cf76a1c17844c2adf8a6ee024506ad
shumpei07 <shumpeikonno07@gmail.com>
2023-06-08 22:01:16 +0900

    デプロイ後修正点

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -6,7 +6,7 @@ class OrderShippingAddress
   with_options presence: true do
     validates :user_id
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :item_id 
     validates :token
     validates :city
